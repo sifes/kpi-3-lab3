e3783159999d83b61b2a0d46087b147ec1cc6c64
ani163 <melnika243@gmail.com>
2025-04-14 21:34:49 +0300

    Add script which moves figure

---
+++ scripts/moving_figure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+step=1
+
+curl -X POST http://localhost:17000 -d "reset"
+curl -X POST http://localhost:17000 -d "white"
+curl -X POST http://localhost:17000 -d "update"
+curl -X POST http://localhost:17000 -d "green"
+curl -X POST http://localhost:17000 -d "figure 0.9 0.1"
+curl -X POST http://localhost:17000 -d "update"
+
+while true; do
+    curl -X POST http://localhost:17000 -d "update"
+    for ((i = 0; i < 16; i += step)); do
+        curl -X POST http://localhost:17000 -d "move -0.05 0.05"
+        curl -X POST http://localhost:17000 -d "update"
+    done
+    for ((i = 0; i < 16; i += step)); do
+        curl -X POST http://localhost:17000 -d "move 0.05 -0.05"
+        curl -X POST http://localhost:17000 -d "update"
+    done
+done
