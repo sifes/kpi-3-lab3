2710c48f6334bb97f8d65e2f6ef1e6f3c8ef6785
ermishins <ermishins@gmail.com>
2025-04-14 15:11:51 +0300

    implemented LBM click event and defaultUI

--- ui/window.go
+++ ui/window.go
@@ -27,6 +27,7 @@ type Visualizer struct {
 
 	sz  size.Event
 	pos image.Rectangle
+	mousePos image.Point
 }
 
 func (pw *Visualizer) Main() {
@@ -43,7 +44,9 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -116,6 +119,10 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	case mouse.Event:
 		if t == nil {
 			// TODO: Реалізувати реакцію на натискання кнопки миші.
+			if e.Button == mouse.ButtonLeft && e.Direction == mouse.DirPress {
+				pw.mousePos = image.Point{X: int(e.X), Y: int(e.Y)}
+				pw.w.Send(paint.Event{})
+			}
 		}
 
 	case paint.Event:
@@ -131,9 +138,24 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 }
 
 func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+	pw.w.Fill(pw.sz.Bounds(), color.RGBA{0, 255, 0, 255}, draw.Src) // Фон
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+
+
+	var centerX, centerY int
+	if pw.mousePos == (image.Point{}) {
+		centerX, centerY = 400, 400
+	} else {
+		centerX, centerY = pw.mousePos.X, pw.mousePos.Y
+	}
+
+	x, y := centerX, centerY
+	blue := color.RGBA{R: 0, G: 0, B: 255, A: 255} // Жовтий колір.
+
+	// Малювання T.
+	pw.w.Fill(image.Rect(x-60, y+140, x+60, y-140), blue, draw.Src)
+	pw.w.Fill(image.Rect(x-150, y, x+150, y-140), blue, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
